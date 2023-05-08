438c7df975b5547b8209ed86355f3d8e9e115454
qooq12315 <qaoo12158@gmail.com>
2023-05-08 11:02:58 +0800

    ok

---
+++ c01/js/add-content.js
@@ -0,0 +1,4 @@
+var my_name = "qooq12315";
+console.log(my_name);
+var first_name = "qoo";
+console.log(first_name);
---
+++ c01/ts/add-content.ts
@@ -0,0 +1,4 @@
+let my_name = "qooq12315";
+console.log(my_name);
+let first_name = "qoo";
+console.log(first_name);
