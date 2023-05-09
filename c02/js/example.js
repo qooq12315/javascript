3dee578f91eaa14ae3741ceeac4ec2b51ad7e16c
qooq12315 <qaoo12158@gmail.com>
2023-05-09 11:43:25 +0800

    ok

--- c02/js/example.js
+++ c02/js/example.js
@@ -17,7 +17,7 @@ let subTotal = tiles * 5;
 let subTotal_element = document.getElementById('subTotal')
 subTotal_element.innerText = '$' + String(subTotal)
 
-let shipping =  7;
+let shipping =  87;
 
 let shipping_element = document.getElementById('shipping')
 shipping_element.innerText = '$' + String(shipping)
