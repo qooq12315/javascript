b6f0c6acd9ad6ccc80dfe5449d43924b37fcaa28
qooq12315 <qaoo12158@gmail.com>
2023-05-09 11:57:46 +0800

    ok

---
+++ c03/js/basic-function.js
@@ -0,0 +1,8 @@
+function updateMessage(message){
+    let message_elment =document.getElementById('message')
+    message_elment.innerText = message
+}
+
+let msg = "加入會員可以得到30% off 的優惠券"
+updateMessage(msg)
+
