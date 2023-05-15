4074e801d96845a6f7b4192e8654fec55a2a3135
qooq12315 <qaoo12158@gmail.com>
2023-05-15 14:08:21 +0800

    ok

---
+++ c06/js/event-attributes.js
@@ -0,0 +1,13 @@
+function checkUsername(){
+    let feedbackElement = document.getElementById('feedback')
+    let usernameElement = document.getElementById('username')
+    let username = usernameElement.value
+    if (username.length < 5) {
+        feedbackElement.textContent = "使用者的名稱必須大於5個字元"
+    } else{
+        feedbackElement.textContent = ""
+    
+    }
+        
+    
+}
