8138eee3c6f19721cf28b2ac3ad78b4fb4766147
qooq12315 <qaoo12158@gmail.com>
2023-05-16 15:17:29 +0800

    ok

---
+++ ajax/js/lesson1.js
@@ -0,0 +1,16 @@
+function reqListener() {
+    let youbikedata = JSON.parse(this.responseText)
+    for(const youbike of youbikedata){
+        console.log(youbike.sna);
+    } 
+}
+
+const windowload = (event) => {
+    console.log('網頁已經全部被載入');
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.send();
+}
+
+window.addEventListener('load', windowload)
