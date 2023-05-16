d2e061ca2e4ac8dfcecc1955c07e9ceecb611284
qooq12315 <qaoo12158@gmail.com>
2023-05-16 15:54:04 +0800

    ok

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,16 +1,21 @@
+let sareaElement =  document.getElementById('sarea')
+
 function reqListener() {
     let youbikedata = JSON.parse(this.responseText)
     let sarea_array = []
     for(const youbike of youbikedata){
-        sarea_array.push(youbike.sarea)
-    } 
+        sarea_array.push(youbike.sarea)    
+    }
     sarea_array = [...new Set(sarea_array)]
-    console.log(sarea_array)
+    for(const area of sarea_array){
+        let optionElement = document.createElement('option')
+        optionElement.textContent = area
+        sareaElement.appendChild(optionElement)
+    }
 }
 
-
 const windowload = (event) => {
-    console.log('網頁已經全部被載入');
+    console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
     req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
