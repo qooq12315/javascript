b1cb83197ac457aad2f90d2eb1fb9dbd38c74893
qooq12315 <qaoo12158@gmail.com>
2023-05-16 16:33:31 +0800

    ok

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,25 +1,26 @@
-let sareaElement = document.getElementById('sarea')
+let sarea_array = []
+let sareaElement = document.getElementById('sarea');
 sareaElement.addEventListener('change', (event) => {
     let selectedIndex = sareaElement.selectedIndex;
-    console.log(sareaElement.options[selectedIndex].value);
-})
+    selectedValue = sareaElement.options[selectedIndex].value
+    if (sarea_array.includes(selectedValue)) {
+        console.log('有這個行政區')
+    }
+});
 
 function reqListener() {
     let youbikedata = JSON.parse(this.responseText)
-    let sarea_array = []
     for (const youbike of youbikedata) {
         sarea_array.push(youbike.sarea)
     }
     sarea_array = [...new Set(sarea_array)]
+    let optionElement = document.createElement('option')
+    optionElement.textContent = '請選擇行政區'
+    sareaElement.appendChild(optionElement)
     for (const area of sarea_array) {
         let optionElement = document.createElement('option')
-        optionElement.textContent = '請選擇行政區'
+        optionElement.textContent = area
         sareaElement.appendChild(optionElement)
-        for (const area of sarea_array) {
-            let optionElement = document.createElement('option')
-            optionElement.textContent = area
-            sareaElement.appendChild(optionElement)
-        }
     }
 }
 
