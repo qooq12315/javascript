c5dbc97365f72c8d3c1b5db461161f3cff2fe55a
qooq12315 <qaoo12158@gmail.com>
2023-05-22 13:46:18 +0800

    ok

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -10,24 +10,37 @@ sareaElement.addEventListener('change', (event) => {
     if (sarea_array.includes(selectedValue)) {
         //console.log(`行政區:${selectedValue}`)
         areaNameElement.innerText = selectedValue
-        let trHTML = ""//行政區內的html資訊
+        let trHTML = "" //行政區內的html資訊
         youbikedata.forEach(element => {
-
             if (element.sarea == selectedValue) {
+                if (element.act == "1") {
+                    var status = "營業中"
+                } else {
+                    var status = "維護中"
+                }
+
                 trHTML += "<tr>"
-                trHTML += "<td>" + element.sna + "</td>"
+                trHTML += "<td>" + element.sna.slice(11) + "</td>"
                 trHTML += "<td>" + element.ar + "</td>"
                 trHTML += "<td>" + element.tot + "</td>"
+                trHTML += "<td>" + element.sbi + "</td>"
                 trHTML += "<td>" + element.bemp + "</td>"
-                trHTML += "<td>" + element.act + "</td>"
+                trHTML += "<td>" + element.updateTime + "</td>"
+                trHTML += "<td>" + status + "</td>"
                 trHTML += "</tr>"
             }
+
         });
         tbodyElement.innerHTML = trHTML
     }
 });
 
 function reqListener() {
+    console.log(this.status)
+    if (this.status != 200) {
+        console.log("網頁維護中...")
+        return
+    }
     youbikedata = JSON.parse(this.responseText)
     for (const youbike of youbikedata) {
         sarea_array.push(youbike.sarea)
@@ -44,11 +57,17 @@ function reqListener() {
     }
 }
 
+function reqError() {
+    console.log("網頁維護中")
+}
+
 const windowload = (event) => {
     console.log('網頁已經全部被載入');
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
-    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.addEventListener("error", reqError)
+    //req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v1/youbike_immediate.json");
     req.send();
 }
 
