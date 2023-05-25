0373ca4937d0896c02e85114bb421b09afa7ca3f
qooq12315 <qaoo12158@gmail.com>
2023-05-25 16:45:45 +0800

    ok

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -1,12 +1,12 @@
 let formElement = document.querySelector('#form');
 
-let allOfDataArray= [];
+let allOfDataArray = [];
 
 const validateName = () => {
     let inputNameElement = document.querySelector('#inputName');
     let nameAlertElement = document.querySelector('#nameAlert');
     let productName = inputNameElement.value
-    if (productName.length == 0 ) {
+    if (productName.length == 0) {
         //console.log('沒有資料');
         nameAlertElement.classList.remove("close")
         return
@@ -14,8 +14,35 @@ const validateName = () => {
     allOfDataArray.push('productName', productName);
 }
 
+const validateCodeFormat = () =>{
+    let inputCodeElement = document.querySelector('#inputCode')
+    let codeAlertElement = document.querySelector('#codeAlert')
+    let inputCodeValue = inputCodeElement.value
+    console.log(inputCodeValue)
+    if (inputCodeValue.length == 0){
+        //console.log("沒有填資料")
+        codeAlertElement.classList.remove("close")
+        return
+    }
+    allOfDataArray.push({'code':inputCodeValue})
+}
+
+const clearAllAlertAndData = ()=>{
+    //清除產品警告
+    let nameAlertElement = document.querySelector('#nameAlert')
+    nameAlertElement.classList.add("close")
+
+    //清除code警告
+    let codeAlertElement = document.querySelector('#codeAlert')
+    codeAlertElement.classList.add("close")
+
+    //清除收集的資料
+    allOfDataArray = []
+}
 formElement.addEventListener('submit', (event) => {
+    clearAllAlertAndData();
     event.preventDefault();
     validateName();
+    validateCodeFormat();
     console.log(allOfDataArray);
 })
