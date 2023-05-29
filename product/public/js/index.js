c77da64db16904a84aad946d9fe4ca767ef935fb
qooq12315 <qaoo12158@gmail.com>
2023-05-29 09:58:07 +0800

    ok

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -1,48 +1,52 @@
-let formElement = document.querySelector('#form');
+let formElement = document.querySelector('#form')
 
 let allOfDataArray = [];
 
-const validateName = () => {
-    let inputNameElement = document.querySelector('#inputName');
-    let nameAlertElement = document.querySelector('#nameAlert');
+const validateName = ()=>{
+    let inputNameElement = document.querySelector('#inputName')
+    let nameAlertElement = document.querySelector('#nameAlert')
     let productName = inputNameElement.value
-    if (productName.length == 0) {
-        //console.log('沒有資料');
+    if (productName.length == 0){
+        //console.log("沒有填資料")
         nameAlertElement.classList.remove("close")
         return
     }
-    allOfDataArray.push('productName', productName);
+    allOfDataArray.push({'productName':productName})
 }
 
 const validateCodeFormat = () =>{
     let inputCodeElement = document.querySelector('#inputCode')
     let codeAlertElement = document.querySelector('#codeAlert')
     let inputCodeValue = inputCodeElement.value
-    console.log(inputCodeValue)
-    if (inputCodeValue.length == 0){
+    
+    //正規則表達式
+    const codePatternReg = /\w\w\w-\w\w\w-\w\w\w/g
+
+    if (inputCodeValue.length == 0 || codePatternReg.test(inputCodeValue) == false){
         //console.log("沒有填資料")
         codeAlertElement.classList.remove("close")
+        console.log("執行錯誤")
         return
     }
     allOfDataArray.push({'code':inputCodeValue})
 }
-
 const clearAllAlertAndData = ()=>{
-    //清除產品警告
+    //清除產品的警告
     let nameAlertElement = document.querySelector('#nameAlert')
     nameAlertElement.classList.add("close")
 
-    //清除code警告
+    //清除code的警告
     let codeAlertElement = document.querySelector('#codeAlert')
     codeAlertElement.classList.add("close")
 
     //清除收集的資料
     allOfDataArray = []
 }
-formElement.addEventListener('submit', (event) => {
-    clearAllAlertAndData();
-    event.preventDefault();
-    validateName();
-    validateCodeFormat();
-    console.log(allOfDataArray);
+
+formElement.addEventListener('submit',(event)=>{
+    clearAllAlertAndData()
+    event.preventDefault()
+    validateName()
+    validateCodeFormat()
+    console.log(allOfDataArray)
 })
