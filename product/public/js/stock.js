24e4a72bfa52800bd31080e902d1213cdd595e1d
qooq12315 <qaoo12158@gmail.com>
2023-05-30 15:00:54 +0800

    ok

--- product/public/js/stock.js
+++ product/public/js/stock.js
@@ -15,7 +15,25 @@ fetch(new Request('codeSearch.json'))
         });
     })
 
-let formElement =document.querySelector('form')
-formElement.addEventListener('submit',(event)=>{
+let formElement = document.querySelector('form')
+
+formElement.addEventListener('submit', (event) => {
     event.preventDefault()
+
+    //取股票ID
+    let stockId = selectedElement.value
+    if (stockId.length > 4) {
+        return
+    }
+
+    //取得年月
+    let monthElement = document.querySelector('input[type="month"]')
+    let fullDateString = monthElement.value
+    fullDateString = fullDateString.replace("-", "")
+    console.log(fullDateString + "01")
+
+    let url = `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${fullDateString}01&stockNo=${stockId}`
+
+    console.log(url)
+
 })
